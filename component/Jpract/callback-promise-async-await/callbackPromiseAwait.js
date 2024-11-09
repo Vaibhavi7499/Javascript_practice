dc1f2670441af30ea2f570a3446b63e5a48510ed
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2024-11-09 13:20:34 +0530

    promise , async await in javascript

--- component/Jpract/callback-promise-async-await/callbackPromiseAwait.js
+++ component/Jpract/callback-promise-async-await/callbackPromiseAwait.js
@@ -1,23 +1,66 @@
-const fetchProducts = (callback) => {
-    setTimeout(() =>{
-        callback([{id:1, productName : 'laptop'}])
+// const fetchProducts = (callback) => {
+//     setTimeout(() =>{
+//         callback([{id:1, productName : 'laptop'}])
+//     })
+// }
+
+// const getDetails = (data,callback) => {
+//     setTimeout(() => {
+//         callback(data)
+//     })
+// }
+
+// const orderProduct = (data,callback) => {
+//     callback(data)
+// }
+
+// fetchProducts((data) => {
+//     getDetails(data,(productD) => {
+//         orderProduct(productD,(orderProduct) => {
+//             console.log(orderProduct)
+//         })
+//     })
+// })
+
+
+const fetchProducts = () => {
+    return new Promise((resolve,reject) => {
+        resolve([{id:1, productName : 'laptop'}])
     })
 }
 
-const getDetails = (data,callback) => {
-    setTimeout(() => {
-        callback(data)
+const getDetails = (data) => {
+    return new Promise((resolve,reject) => {
+        resolve(data)
     })
 }
 
-const orderProduct = (data,callback) => {
-    callback(data)
+const orderProduct = (data) => {
+    return new Promise((resolve,reject)=>{
+        resolve(data)
+    })
 }
 
-fetchProducts((data) => {
-    getDetails(data,(productD) => {
-        orderProduct(productD,(orderProduct) => {
-            console.log(orderProduct)
-        })
-    })
-})
+// fetchProducts().then((products) => {
+//     return getDetails(products)
+// }).then((cart) => {
+//     return orderProduct(cart)
+// }).then((order) => {
+//     console.log(order)
+// }).catch((err) => { console.log('err',err)})
+
+
+createOrder = async () => {
+ try {
+    const products = await fetchProducts();
+
+    const cart = await getDetails(products)
+
+    const order = await orderProduct(cart)
+    console.log(order,'async await')
+ } catch (error) {
+    console.error("Error",error)
+ }
+}
+
+createOrder()
